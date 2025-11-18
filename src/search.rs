db63864ec85a027fff3b6f20d4534855b8c03378
Daniel Bulant <danbulant@gmail.com>
2025-11-18 23:02:37 +0100

    Fix symlink handling (#2)
    * simply symlink handling
    
    * Style: `!is_some` → `is_none`
    
    ---------
    
    Co-authored-by: Ridan Vandenbergh <ridanvandenbergh@gmail.com>

--- src/search.rs
+++ src/search.rs
@@ -146,7 +146,9 @@ impl IconSearch<Initial> {
             .flatten() // merge all the iterators
             .flatten() // remove Err entries
             .filter_map(|entry| Some((entry.file_type().ok()?, entry))) // get file type for each entry and skip if fail
-            .partition::<Vec<_>, _>(|(ft, _)| ft.is_dir());
+            .partition::<Vec<_>, _>(|(ft, entry)| {
+                ft.is_dir() || (entry.path().extension().is_none() && ft.is_symlink())
+            });
 
         // icons at the top-level in a base_dir don't belong to a theme, but must still be able to be found!
         let files = files
